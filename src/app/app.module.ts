bac2b2ce494cfda6ced75b22e602cee97c973bcd
Pedro <russopedro87@gmail.com>
2021-06-30 19:53:16 -0300

    Styling register component. Reordering components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginModule } from './login/login.module';
+import { LoginModule } from './components/auth/login/login.module';
 
 @NgModule({
   declarations: [
