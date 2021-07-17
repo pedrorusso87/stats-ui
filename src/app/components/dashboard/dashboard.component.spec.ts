8f1f4f996118dd11b96af52249dbc6ae323ea336
Pedro <russopedro87@gmail.com>
2021-07-16 23:06:58 -0300

    fixing unit test for app

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LocalStorage, LocalStorageService } from 'ngx-webstorage';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  class LocalStorageStub {
+    constructor(){}
+    retrieve(key: string) {}
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [{
+        provide: LocalStorageService,
+        useValue: new LocalStorageStub()
+      }]
     })
     .compileComponents();
   });
