73d574ff083af61f20c8c28290a9d50eee641d50
Pedro Russo <russopedro87@gmail.com>
2021-09-28 21:31:38 -0300

    Adding background image to site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
-import { StatsComponent } from './components/stats/stats.component';
+import { StatsComponent } from './components/stats-page/stats.component';
 
 const routes: Routes = [
   {
