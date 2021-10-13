e7ae030d017ffba4e9998936a2df696ed8ed0ae7
Pedro <russopedro87@gmail.com>
2021-10-13 19:38:01 -0300

    Teams component first draft

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,13 +5,15 @@ import { MatSelectModule } from '@angular/material/select';
 import { StatsPageModule } from './stats-page/stats-page.module';
 import { HomeComponent } from './home/home.component';
 import { TeamsComponent } from './teams/teams.component';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   declarations: [HomeComponent, TeamsComponent],
   imports: [
     CommonModule,
     MatCardModule,
     MatSelectModule,
-    StatsPageModule
+    StatsPageModule,
+    MatTableModule
   ]
 })
 export class ComponentsModule { }
